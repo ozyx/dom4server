340004b4a4ad42e3906a12be409cb2c41d14c255
ozyx <jessemazzella@gmail.com>
2016-12-17 11:07:04 -0800

    play around with simple server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,44 @@
 import { DomServer } from "./DomServer";
 import { DomConfig } from "./DomConfig";
+import * as fs from "fs";
+import * as http from "http";
+import * as path from "path";
 
-let config: DomConfig = require("../config.json");
-let domServer: DomServer = new DomServer(config, "./");
+let dispatch = require("dispatch");
+// let config: DomConfig = require("../config.json");
+// let domServer: DomServer = new DomServer(config, "./");
 
-domServer.version();
-domServer.start();
+// domServer.version();
+// domServer.start();
+
+const port = 4200;
+
+let server = http.createServer(dispatch({
+    "/": {
+        GET: (req: any, res: any) => {
+            fs.readFile("./src/index.html", (err: any, data: any) => {
+                if (err) {
+                    console.error(err.message);
+                } else {
+                    res.writeHead(200, {
+                        "Content-Type": "text/html",
+                        "Content-Length": data.length
+                    });
+                    res.write(data);
+                    res.end();
+                }
+            });
+        }
+    },
+    "/launch": {
+        GET: (req: any, res: any) => {
+            console.log("launch");
+            res.end();
+        }
+    }
+}));
+
+// start server
+server.listen(port, () => {
+    console.log(`Server listening on: http://localhost:${port}`);
+});
