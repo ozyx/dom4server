2c7ce13a07a7ed3769359cfa318a5104b0fd5f97
ozyx <jessemazzella@gmail.com>
2016-12-17 08:48:07 -0800

    remove prompt

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { DomServer } from "./DomServer";
 import { DomConfig } from "./DomConfig";
-let prompt: any = require("prompt");
+
 let config: DomConfig = require("../config.json");
 let domServer: DomServer = new DomServer(config, "./");
 
