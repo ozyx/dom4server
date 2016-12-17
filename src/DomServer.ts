73437656ff5119f5baa6cc2cacc0ad93f9187b37
ozyx <jessemazzella@gmail.com>
2016-12-16 21:29:23 -0800

    launch server

--- src/DomServer.ts
+++ src/DomServer.ts
@@ -1,6 +1,6 @@
 import * as path from "path";
 import * as child_process from "child_process";
-
+import { DomConfig } from "./DomConfig";
 export class DomServer {
     private dom4: string;
 
@@ -8,12 +8,39 @@ export class DomServer {
         this.dom4 = path.resolve(`${domPath}dom4.sh`);
     }
 
+    private launch(config: DomConfig) {
+        let dominions = child_process.spawn(`${this.dom4}`,
+            [
+                config.title,
+                "--port", `${config.port}`,
+                "-T",
+                "-S",
+                "--noclientstart",
+                "--era", `${config.era}`
+            ]);
+
+        dominions.stdout.on("data", (data: any) => {
+            console.log(data.toString());
+        });
+
+        dominions.stderr.on("data", (data: any) => {
+            console.log(data.toString());
+        });
+
+        dominions.on("exit", (code) => {
+            console.log(`exited with code ${code}`);
+        });
+
+    }
+
     public version(): void {
         let dominions = child_process.spawn(`${this.dom4}`, ["--version"]);
         dominions.stdout.on("data", (data) => {
             console.log(data.toString());
         });
+    }
 
-
+    public start(config: DomConfig) {
+        this.launch(config);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { DomServer } from "./DomServer";
-
+import { DomConfig } from "./DomConfig";
+let config: DomConfig = require("../config.json");
 let domServer: DomServer = new DomServer("./");
 
 domServer.version();
+domServer.start(config);
